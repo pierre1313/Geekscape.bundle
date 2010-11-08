6a71743308c5b9d90f223a5b4f9e9e5e60d8568a
Pierre Della Nave <p.dellanave@gmail.com>
2010-11-08 13:31:24 -0800

    Fixed plugin by using RSS feed + conversion to V1 Framework

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,20 +1,40 @@
-import PMS
-from PMS import Plugin, Prefs, Log, XML, HTTP, JSON, RSS, Utils
-from PMS.MediaXML import *
-from PMS.Shorthand import _L, _R, _E, _D
+# PMS plugin framework
+from PMS import *
+from PMS.Objects import *
+from PMS.Shortcuts import *
+
+####################################################################################################
+
+VIDEO_PREFIX = "/video/geekscape"
+
+NAME = L('Title')
+
+# make sure to replace artwork with what you want
+# these filenames reference the example files in
+# the Contents/Resources/ folder in the bundle
+ART           = 'art-default.png'
+ICON          = 'icon-default.png'
+
+####################################################################################################
 
 def Start():
-  Plugin.AddRequestHandler("/video/geekscape", HandleRequest, _L("Geekscape"), "icon-default.png", "art-default.png")
-  Plugin.AddViewGroup("Menu", viewMode="InfoList", contentType="items")
-  
-def HandleRequest(pathNouns, count):
-  if count == 0:
-    dir = MediaContainer(title1=_L("Geekscape"), art="art-default.png", viewGroup="Menu")
-    page = XML.ElementFromString(HTTP.GetCached("http://www.geekscape.net/podcasts/index.php", 7200), True)
-    for episode in page.xpath("//div[@class='episode-list']"):
-      title = episode.xpath("dl/dt/a")[0].text
-      thumb = episode.xpath("img")[0].get("src")
-      summary = episode.xpath("dl/dd[1]")[0].text
-      url = episode.xpath("dl/dd/a[2]")[0].get("href")
-      dir.AppendItem(VideoItem(url, title, summary, "", thumb))
-    return dir.ToXML()
+    Plugin.AddPrefixHandler(VIDEO_PREFIX, VideoMainMenu, L('Title'), ICON, ART)
+
+    Plugin.AddViewGroup("InfoList", viewMode="InfoList", mediaType="items")
+    Plugin.AddViewGroup("List", viewMode="List", mediaType="items")
+
+    MediaContainer.art = R(ART)
+    MediaContainer.title1 = NAME
+    DirectoryItem.thumb = R(ICON)
+
+
+def VideoMainMenu():
+
+  dir = MediaContainer(title1="Geekscape", art=R(ART), viewGroup="List")
+  page = XML.ElementFromURL("http://pod.geekscape.tv/Geekscape-Video.xml",cacheTime=7200)
+  for episode in page.xpath("//item"):
+    title = episode.xpath("title")[0].text
+    summary = episode.xpath("description")[0].text
+    url = episode.xpath("enclosure")[0].get("url")
+    dir.Append(VideoItem(url, title, summary, "", thumb=R(ICON)))
+  return dir
